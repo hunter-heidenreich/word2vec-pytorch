8ecd29280f4c5a4af8c36debe753aa80a112f0ab
Hunter Heidenreich <hheidenreich@g.harvard.edu>
2024-03-25 00:01:20 -0400

    Basic data and tokenizer

--- word2vec/data.py
+++ word2vec/data.py
@@ -1,5 +1,8 @@
 from datasets import load_dataset
 from torch.utils.data import Dataset
+from tqdm import tqdm
+
+from word2vec.tokenizer import get_tokenizer, add_spaces_around_foreign_characters
 
 
 class WindowedDatasets(Dataset):
@@ -9,21 +12,34 @@ class WindowedDatasets(Dataset):
         dataset_name: str = 'wikitext',
         subset: str = 'wikitext-2-raw-v1',
         split: str = 'train',
-        window_size: int = 2,
+        window_size: int = 5,
     ):
-        self.dataset = load_dataset(dataset_name, subset, split=split)
         self.window_size = window_size
+        self.dataset = load_dataset(dataset_name, subset, split=split)['text']
+        self.dataset = [add_spaces_around_foreign_characters(text) for text in self.dataset if text.strip()]
         
-        import code 
-        code.interact(local=locals())
-
+        # get tokenizer
+        self.tokenizer = get_tokenizer(data=self.dataset, dataset_name=dataset_name, subset=subset)
+        
+        # encode dataset
+        self.centers = []
+        self.contexts = []
+        for text in tqdm(self.dataset, desc='Encoding dataset'):
+            encoding = self.tokenizer.encode(text)
+            datum = encoding.ids
+            # pre- and post-pad with <pad> token
+            datum = [self.tokenizer.token_to_id('<pad>')] * self.window_size + datum + [self.tokenizer.token_to_id('<pad>')] * self.window_size
+            for i in range(self.window_size, len(datum) - self.window_size):
+                center = datum[i]
+                context = datum[i - self.window_size:i] + datum[i + 1:i + self.window_size + 1]
+                self.centers.append(center)
+                self.contexts.append(context)
+            
     def __len__(self):
-        return len(self.data)
-
+        return len(self.centers)
+    
     def __getitem__(self, idx):
-        target = self.data[idx]
-        context = self.data[max(0, idx - self.window_size):idx] + self.data[idx + 1:idx + self.window_size + 1]
-        return context, target
+        return self.centers[idx], self.contexts[idx]
 
 
 if __name__ == '__main__':
--- word2vec/tokenizer.py
+++ word2vec/tokenizer.py
@@ -1,36 +1,66 @@
-from typing import Optional
+import os
+import unicodedata
+from typing import Iterable, Optional
 
-from datasets import load_dataset, Dataset
-from tokenizers import Tokenizer 
-from tokenizers import trainers, normalizers, models, pre_tokenizers
+from datasets import load_dataset
+from tokenizers import Tokenizer
+from tokenizers import trainers, normalizers, models, pre_tokenizers, processors
+
+
+def add_spaces_around_foreign_characters(text: str) -> str:
+    """Add spaces around foreign characters in a text.
+    
+    Args:
+        text: The text.
+        
+    Returns:
+        The text with spaces around foreign characters.
+    """
+    new_text = []
+    for char in text:
+        # Check character category
+        cat = unicodedata.category(char)
+        # Letter categories start with 'L'
+        if cat.startswith('L') and ord(char) >= 128:
+            new_text.append(" " + char + " ")
+        else:
+            new_text.append(char)
+    return ''.join(new_text)
 
 
 def get_tokenizer(
-    data: Optional[Dataset] = None,
+    data: Optional[Iterable[str]] = None,
     dataset_name: Optional[str] = 'wikitext',
     subset: Optional[str] = 'wikitext-2-raw-v1',
     vocab_size: int = 1_000_000,
+    min_frequency: int = 2,
 ) -> Tokenizer:
     """Get a tokenizer for a dataset. If the tokenizer is not found, train a new one.
     
     Args:
+        data: The dataset.
         dataset_name: The name of the dataset.
         subset: The subset of the dataset.
         vocab_size: The size of the vocabulary.
+        min_frequency: The minimum frequency of a token to be included in the vocabulary.
         
     Returns:
         The tokenizer.
     """
+    if not os.path.exists('tokenizers'):
+        os.makedirs('tokenizers')
+    
     size_str_mil = f'{vocab_size // 1_000_000}M' if vocab_size >= 1_000_000 else f'{vocab_size}'
     try:
-        tokenizer = Tokenizer.from_file(f'tokenizers/{subset}-vocab_{size_str_mil}.json')
+        tokenizer = Tokenizer.from_file(f'tokenizers/{subset}-vocab_{min_frequency}_{size_str_mil}.json')
         print('Loaded. Vocabulary size:', tokenizer.get_vocab_size())
         return tokenizer
     except Exception:
         pass
     
     if data is None:
-        data = load_dataset(dataset_name, subset, split='train')
+        data = load_dataset(dataset_name, subset, split='train')['text']
+        data = [add_spaces_around_foreign_characters(text) for text in data if len(text) > 0]
     
     # Initialize a tokenizer
     tokenizer = Tokenizer(
@@ -46,14 +76,25 @@ def get_tokenizer(
         pre_tokenizers.Digits(individual_digits=True),
         pre_tokenizers.Whitespace(),
     ])
+    special_tokens = [
+        ("<unk>", 0),
+        ("<pad>", 1),
+        ("<eos>", 2),
+        ("<sos>", 3),
+    ]
+    tokenizer.post_processor = processors.TemplateProcessing(
+        single="<sos> $A <eos>",
+        special_tokens=special_tokens,
+    )
     tokenizer.train_from_iterator(
-        data['text'], 
+        data,
         trainer=trainers.WordLevelTrainer(
             vocab_size=vocab_size,
+            min_frequency=min_frequency,
             special_tokens=['<unk>', '<pad>', '<eos>', '<sos>'],
             show_progress=True,
         ),
     )
-    tokenizer.save(f'tokenizers/{subset}-vocab_{size_str_mil}.json')
+    tokenizer.save(f'tokenizers/{subset}-vocab_{min_frequency}_{size_str_mil}.json')
     print('Trained. Vocabulary size:', tokenizer.get_vocab_size())
     return tokenizer
